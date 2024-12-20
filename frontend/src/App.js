731688a0e508a305f6a91d2600c841dca1b14fd6
darioduarte1 <darioduarte1991@gmail.com>
2024-12-20 15:56:28 +0100

    hotfix/login - ir para la area segura

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Logout from './pages/Logout';
 import Home from './pages/Home';
 
 function App() {
-  return (
-      <Router>
-          <Routes>
-              <Route path="/register" element={<Register />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/logout" element={<Logout />} />
-              <Route path="/home" element={<Home />} />
-          </Routes>
-      </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+
+                {/* Ruta raíz redirige a /login */}
+                    <Route path="/" element={<Navigate to="/login" />} />
+
+                {/* Rutas específicas */}
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/logout" element={<Logout />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
