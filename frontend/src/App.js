9857e506585ae58de496d6f066df116ca5536f2f
darioduarte1 <darioduarte1991@gmail.com>
2024-12-22 12:10:57 +0100

    despliegue del front para git pages - OK

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css"; // Importa los estilos de react-toastify
+import "react-toastify/dist/ReactToastify.css";
 import LoginRegister from "./pages/LoginRegister";
 import Logout from "./pages/Logout";
 import Home from "./pages/Home";
 
 function App() {
     return (
-        <>
-            <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-                <Routes>
-                    {/* Redirección de la ruta raíz */}
-                    <Route path="/" element={<Navigate to="/auth" replace />} />
-
-                    {/* Rutas específicas */}
-                    <Route path="/auth" element={<LoginRegister />} />
-                    <Route path="/home" element={<Home />} />
-                    <Route path="/logout" element={<Logout />} />
-                </Routes>
-            </Router>
-
-            {/* Contenedor de Toast para notificaciones */}
-            <ToastContainer 
+        <Router>
+            <Routes>
+                <Route path="/" element={<Navigate to="/auth" replace />} />
+                <Route path="/auth" element={<LoginRegister />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/logout" element={<Logout />} />
+            </Routes>
+            <ToastContainer
                 position="top-right"
                 autoClose={5000}
                 hideProgressBar={false}
@@ -34,7 +27,7 @@ function App() {
                 pauseOnHover
                 theme="light"
             />
-        </>
+        </Router>
     );
 }
 
