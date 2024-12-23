c1f01cdaf64e1bb4b5f1c95a97049dce674e26f3
darioduarte1 <darioduarte1991@gmail.com>
2024-12-23 14:21:58 +0100

    Recolección de archivos estáticos y ajustes finales

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,16 +5,25 @@ import "react-toastify/dist/ReactToastify.css";
 import LoginRegister from "./pages/LoginRegister";
 import Logout from "./pages/Logout";
 import Home from "./pages/Home";
+import Home2 from "./components/Home2"; // Importando Home2
 
 function App() {
     return (
         <Router>
             <Routes>
+                {/* Redirección de la raíz a la página de autenticación */}
                 <Route path="/" element={<Navigate to="/auth" replace />} />
+
+                {/* Rutas existentes */}
                 <Route path="/auth" element={<LoginRegister />} />
                 <Route path="/home" element={<Home />} />
                 <Route path="/logout" element={<Logout />} />
+
+                {/* Nueva ruta para el componente Home2 */}
+                <Route path="/home2" element={<Home2 />} />
             </Routes>
+            
+            {/* Configuración de notificaciones */}
             <ToastContainer
                 position="top-right"
                 autoClose={5000}
