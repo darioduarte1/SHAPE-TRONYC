e1fb392e633b65c64bf052d26201787ab2106efe
darioduarte1 <darioduarte1991@gmail.com>
2024-12-27 00:24:18 +0100

    hotfix/pylint 10/10 eliminacion errores profiles

--- backend/profiles/apps.py
+++ backend/profiles/apps.py
@@ -1,6 +1,14 @@
+"""
+Configuration for the profiles app.
+
+This module defines the application configuration for the profiles app in Django.
+"""
 from django.apps import AppConfig
 
 class ProfilesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+    """
+    Configuration class for the profiles app.
+
+    This class sets the name of the application to 'profiles'.
+    """
     name = 'backend.profiles'
-    
--- backend/profiles/serializers.py
+++ backend/profiles/serializers.py
@@ -1,8 +1,31 @@
+"""
+Serializadores para la app profiles.
+
+Este módulo define el serializador para el modelo UserProfile, permitiendo convertir
+las instancias del modelo en representaciones JSON y viceversa para su uso en las vistas de API.
+"""
+
 from rest_framework import serializers
 from .models import UserProfile
 
-class UserProfileSerializer(serializers.ModelSerializer):
+class UserProfileSerializer(serializers.ModelSerializer):  # pylint: disable=too-few-public-methods
+    """
+    Serializador para el modelo UserProfile.
+
+    Este serializador transforma las instancias del modelo UserProfile a un formato JSON
+    y también valida y deserializa datos JSON para crear o actualizar instancias del modelo.
+
+    Atributos:
+        Meta (class): Define el modelo y los campos que se incluirán en la representación
+        serializada.
+    """
     class Meta:
+        """
+        Metaclase que configura el serializador.
+
+        Atributos:
+            model (UserProfile): El modelo que será serializado.
+            fields (list): Los campos del modelo que se incluirán en la serialización.
+        """
         model = UserProfile
         fields = ['user', 'full_name', 'age', 'gender', 'profile_picture', 'contact_number']
-        
