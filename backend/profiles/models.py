2c04c30389e6906f6a0c35daaee94dcd6f79c9db
darioduarte1 <darioduarte1991@gmail.com>
2024-12-28 04:53:25 +0100

    hotfix/Cargamento de la foto de Perfil actualizado y funcional trabajando con cloudinary - OK

--- backend/profiles/models.py
+++ backend/profiles/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import User  # pylint: disable=imported-auth-user
+from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from cloudinary.models import CloudinaryField
 
 class UserProfile(models.Model):
     """
@@ -13,7 +12,7 @@ class UserProfile(models.Model):
         full_name (str): Nombre completo del usuario.
         age (int): Edad del usuario.
         gender (str): Género del usuario, con opciones 'Male', 'Female' y 'Other'.
-        profile_picture (CloudinaryField): Imagen de perfil del usuario.
+        profile_picture (URLField): URL de la imagen de perfil del usuario.
         contact_number (str): Número de contacto del usuario.
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -25,43 +24,43 @@ class UserProfile(models.Model):
         null=True,
         blank=True
     )
-    profile_picture = CloudinaryField('image', blank=True, null=True, max_length=255)
+    profile_picture = models.URLField(max_length=500, blank=True, null=True)  # Cambiado a URLField
     contact_number = models.CharField(max_length=15, null=True, blank=True)
 
     def __str__(self):
         """
         Devuelve una representación en cadena del perfil del usuario.
 
-        Si el nombre completo está disponible, lo devuelve. De lo contrario, devuelve el nombre
-        de usuario.
+        Si el nombre completo está disponible, lo devuelve. De lo contrario,
+        devuelve el nombre de usuario o un valor por defecto.
+
+        Returns:
+            str: Nombre completo, nombre de usuario o 'Sin nombre'.
         """
-        return str(self.full_name if self.full_name else self.user.username)  # pylint: disable=no-member
+        return str(self.full_name or self.user.username or "Sin nombre")
 
-
-# Señales para crear y guardar automáticamente el perfil de usuario
 @receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):  # pylint: disable=unused-argument
+def create_user_profile(sender, instance, created, **kwargs):
     """
     Crea automáticamente un perfil asociado al usuario al momento de su creación.
 
-    Argumentos:
+    Args:
         sender (type): Modelo que envía la señal, en este caso User.
         instance (User): Instancia del modelo User que disparó la señal.
         created (bool): Indica si el usuario fue creado.
         **kwargs: Argumentos adicionales que no se usan en esta función.
     """
     if created:
-        UserProfile.objects.create(user=instance)  # pylint: disable=no-member
-
+        UserProfile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):  # pylint: disable=unused-argument
+def save_user_profile(sender, instance, **kwargs):
     """
     Guarda automáticamente el perfil asociado al usuario.
 
-    Argumentos:
+    Args:
         sender (type): Modelo que envía la señal, en este caso User.
         instance (User): Instancia del modelo User que disparó la señal.
         **kwargs: Argumentos adicionales.
     """
-    instance.userprofile.save()  # pylint: disable=no-member
+    instance.userprofile.save()
