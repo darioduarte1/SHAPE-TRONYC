a38933fdc06346e7925025353884662830e4e07f
darioduarte1 <darioduarte1991@gmail.com>
2024-12-29 18:51:56 +0100

    feature/Anadido Boton de Inicio en DropDown - OK

--- frontend/src/pages/Security.jsx
+++ frontend/src/pages/Security.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Header from "../components/Header";
 import "../styles/Security.css";
 
 const Security = () => {
@@ -53,117 +54,120 @@ const Security = () => {
   };
 
   return (
-    <div className="security-container">
-      <h1 className="security-title">{t.pageTitle}</h1>
-      <div className="accordion">
-        {/* Change Password */}
-        <div className="accordion-item">
-          <button
-            className="accordion-button"
-            onClick={() => toggleSection("password")}
-          >
-            {t.changePassword}
-          </button>
-          {activeSection === "password" && (
-            <div className="accordion-content">
-              <form>
-                <input
-                  type="password"
-                  placeholder={t.currentPassword}
-                  className="security-input"
-                />
-                <input
-                  type="password"
-                  placeholder={t.newPassword}
-                  className="security-input"
-                />
-                <input
-                  type="password"
-                  placeholder={t.confirmPassword}
-                  className="security-input"
-                />
-                <button type="submit" className="security-submit-button">
-                  {t.changePassword}
-                </button>
-              </form>
-            </div>
-          )}
-        </div>
-
-        {/* Change Email */}
-        <div className="accordion-item">
-          <button
-            className="accordion-button"
-            onClick={() => toggleSection("email")}
-          >
-            {t.changeEmail}
-          </button>
-          {activeSection === "email" && (
-            <div className="accordion-content">
-              <form>
-                <input
-                  type="email"
-                  placeholder={t.newEmail}
-                  className="security-input"
-                />
-                <button type="submit" className="security-submit-button">
-                  {t.changeEmail}
-                </button>
-              </form>
-            </div>
-          )}
-        </div>
-
-        {/* Change Language */}
-        <div className="accordion-item">
-          <button
-            className="accordion-button"
-            onClick={() => toggleSection("language")}
-          >
-            {t.changeLanguage}
-          </button>
-          {activeSection === "language" && (
-            <div className="accordion-content">
-              <div className="language-options">
-                <button
-                  className={`language-button ${
-                    selectedLanguage === "en" ? "selected" : ""
-                  }`}
-                  onClick={() => handleLanguageChange("en")}
-                >
-                  <img
-                    src="https://res.cloudinary.com/deizebh0z/image/upload/v1735434595/pictures_backend/flags_register/English.png"
-                    alt="English"
+    <>
+      <Header />
+      <div className="security-container">
+        <h1 className="security-title">{t.pageTitle}</h1>
+        <div className="accordion">
+          {/* Change Password */}
+          <div className="accordion-item">
+            <button
+              className="accordion-button"
+              onClick={() => toggleSection("password")}
+            >
+              {t.changePassword}
+            </button>
+            {activeSection === "password" && (
+              <div className="accordion-content">
+                <form>
+                  <input
+                    type="password"
+                    placeholder={t.currentPassword}
+                    className="security-input"
                   />
-                </button>
-                <button
-                  className={`language-button ${
-                    selectedLanguage === "es" ? "selected" : ""
-                  }`}
-                  onClick={() => handleLanguageChange("es")}
-                >
-                  <img
-                    src="https://res.cloudinary.com/deizebh0z/image/upload/v1735434595/pictures_backend/flags_register/Spanish.png"
-                    alt="Español"
+                  <input
+                    type="password"
+                    placeholder={t.newPassword}
+                    className="security-input"
                   />
-                </button>
-                <button
-                  className={`language-button ${
-                    selectedLanguage === "pt" ? "selected" : ""
-                  }`}
-                  onClick={() => handleLanguageChange("pt")}
-                >
-                  <img
-                    src="https://res.cloudinary.com/deizebh0z/image/upload/v1735434595/pictures_backend/flags_register/Portuguese.png"
-                    alt="Português"
+                  <input
+                    type="password"
+                    placeholder={t.confirmPassword}
+                    className="security-input"
                   />
-                </button>
+                  <button type="submit" className="security-submit-button">
+                    {t.changePassword}
+                  </button>
+                </form>
               </div>
-            </div>
-          )}
+            )}
+          </div>
+
+          {/* Change Email */}
+          <div className="accordion-item">
+            <button
+              className="accordion-button"
+              onClick={() => toggleSection("email")}
+            >
+              {t.changeEmail}
+            </button>
+            {activeSection === "email" && (
+              <div className="accordion-content">
+                <form>
+                  <input
+                    type="email"
+                    placeholder={t.newEmail}
+                    className="security-input"
+                  />
+                  <button type="submit" className="security-submit-button">
+                    {t.changeEmail}
+                  </button>
+                </form>
+              </div>
+            )}
+          </div>
+
+          {/* Change Language */}
+          <div className="accordion-item">
+            <button
+              className="accordion-button"
+              onClick={() => toggleSection("language")}
+            >
+              {t.changeLanguage}
+            </button>
+            {activeSection === "language" && (
+              <div className="accordion-content">
+                <div className="language-options">
+                  <button
+                    className={`language-button ${
+                      selectedLanguage === "en" ? "selected" : ""
+                    }`}
+                    onClick={() => handleLanguageChange("en")}
+                  >
+                    <img
+                      src="https://res.cloudinary.com/deizebh0z/image/upload/v1735434595/pictures_backend/flags_register/English.png"
+                      alt="English"
+                    />
+                  </button>
+                  <button
+                    className={`language-button ${
+                      selectedLanguage === "es" ? "selected" : ""
+                    }`}
+                    onClick={() => handleLanguageChange("es")}
+                  >
+                    <img
+                      src="https://res.cloudinary.com/deizebh0z/image/upload/v1735434595/pictures_backend/flags_register/Spanish.png"
+                      alt="Español"
+                    />
+                  </button>
+                  <button
+                    className={`language-button ${
+                      selectedLanguage === "pt" ? "selected" : ""
+                    }`}
+                    onClick={() => handleLanguageChange("pt")}
+                  >
+                    <img
+                      src="https://res.cloudinary.com/deizebh0z/image/upload/v1735434595/pictures_backend/flags_register/Portuguese.png"
+                      alt="Português"
+                    />
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
