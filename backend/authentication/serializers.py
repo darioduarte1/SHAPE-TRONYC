0a5f5c20fb96d6ac579e539fc8e0654b7aeb7e8b
darioduarte1 <darioduarte1991@gmail.com>
2025-01-02 14:03:11 +0100

    hotfix/Correcion errores Fluxo Registro Normal Usuario Paso 2 - Verificar si el usuario ya existe en la base de datos - OK Paso 3 - Crear usuario y enviar email de verificación - OK Paso 4 - Activación del correo electrónico - OK

--- backend/authentication/serializers.py
+++ backend/authentication/serializers.py
@@ -6,8 +6,41 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ['username', 'email', 'password', 'language']
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'username': {'error_messages': {'unique': "El nombre de usuario ya está en uso."}},
+            'email': {'error_messages': {'unique': "El correo electrónico ya está registrado."}},
+        }
+
+    def validate(self, data):
+        language = data.get('language', 'en')  # Obtener el idioma del usuario o usar inglés por defecto
+        translations = {
+            'en': {
+                'username_exists': "A user with that username already exists.",
+                'email_exists': "The email is already registered.",
+            },
+            'es': {
+                'username_exists': "El nombre de usuario ya está en uso.",
+                'email_exists': "El correo electrónico ya está registrado.",
+            },
+            'pt': {
+                'username_exists': "O nome de usuário já está em uso.",
+                'email_exists': "O e-mail já está registrado.",
+            },
+        }
+
+        # Verificar username y email manualmente
+        if User.objects.filter(username=data['username']).exists():
+            raise serializers.ValidationError({
+                'username': translations[language].get('username_exists', "Error")
+            })
+        if User.objects.filter(email=data['email']).exists():
+            raise serializers.ValidationError({
+                'email': translations[language].get('email_exists', "Error")
+            })
+
+        return data
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
